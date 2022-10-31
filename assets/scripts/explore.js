6c796d280f21457244f3b05d871dbc65bccf5a95
Nathan Karter <nathanielkarter@gmail.com>
2022-10-30 22:48:11 -0700

    d

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,30 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
 
+    const synth = window.speechSynthesis;
+    let voices = [];
+
+    const voiceSelect = document.querySelector('select');
+    const inputTxt = document.querySelector('textarea');
+
+    function populateVoiceList() {
+        
+        voices = synth.getVoices();
+      
+        for (let i = 0; i < voices.length ; i++) {
+          const option = document.createElement('option');
+          option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      
+          if (voices[i].default) {
+            option.textContent += ' — DEFAULT';
+          }
+      
+          option.setAttribute('data-lang', voices[i].lang);
+          option.setAttribute('data-name', voices[i].name);
+          voiceSelect.appendChild(option);
+        }
+      }
+      
+      populateVoiceList();
+      alert(voices.length);
 }
